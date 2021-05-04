943a8395e0dda635ec348b8c557892d09b6d2e8b
Miguel Olmos Mares <miguel.olmosmares@gmail.com>
2021-05-04 13:55:25 -0500

    Verificacion de logstash de acuerdo con el manual de referencia

--- volumes/logstash/config/queries/incremental.sql
+++ volumes/logstash/config/queries/incremental.sql
@@ -1,9 +1,8 @@
 SELECT
-	j.journal_id, j.sale_ID ,j.action_type,
-	s.sale_ID, s.officeCode, s.salesKey, s.destination, s.saleDate, s.serviceClass,
-	s.serviceNumber, s.serviceTime, s.origin, s.serviceDate, s.category, s.price, s.saleTime, s.discount
+		j.journal_id, j.sale_ID ,j.action_type,
+		s.sale_ID, s.officeCode, s.salesKey, s.destination, s.saleDate, s.serviceClass, s.serviceNumber, s.serviceTime, s.origin, s.serviceDate, s.category, s.price, s.saleTime, s.discount
 FROM classicmodels.gfaSales_journal j
 LEFT JOIN classicmodels.gfaSales s ON s.sale_ID = j.sale_ID
 WHERE j.journal_id > :sql_last_value
-	AND j.action_time < NOW()
+		AND j.action_time < NOW()
 ORDER BY j.journal_id
